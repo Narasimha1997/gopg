91103ecfdf7d1d5de20d92ee90a97bc38d49f21b
Narasimha1997 <yogaimpression@gmail.com>
2021-01-11 21:32:51 +0530

    Added sandbox realization example in example.go

--- examples/example.go
+++ examples/example.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"runtime"
 )
 
 func main() {
 	fmt.Println("Hello, world!!")
 	fmt.Println(runtime.GOOS)
+
+	files, _ := ioutil.ReadDir("/")
+
+	fmt.Println("Files :")
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
